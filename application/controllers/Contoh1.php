427dc1ae092e6ad1257135a7bc871e8ddef4da2d
Nisa-Nur-Herawati <nisanurherawati6@gmail.com>
2021-09-26 19:30:49 +0700

    Upload awal project

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Nisa Nur Herawati
+        saya tinggal di daerah Kulon Progo
+        Olahraga yang saya sukai adalah Bulutangkis";
+    }
+}
