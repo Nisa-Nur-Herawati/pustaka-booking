be02c8723bcc4bbb6b24a7eec3f4be264f0c5648
Nisa-Nur-Herawati <nisanurherawati6@gmail.com>
2021-10-12 10:41:39 +0700

    Membuat Template web sederhana

---
+++ application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan   : Nisa</li>
+        <li>Nama Belakang: Nur Herawati</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Turusan, pendoworejo, Girimulyo, Kulon Progo</li>
+    </ul>
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Bekasi</li>
+    </ul>
+    
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+    </ul>
+</section>
